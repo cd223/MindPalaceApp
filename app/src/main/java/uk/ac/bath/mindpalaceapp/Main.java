ae3c920450fc4f5eed72b61642e86b1898c54b2a
cd223 <chrisdavies870@gmail.com>
2018-09-30 18:14:23 +0100

    Adding Estimote Android SDK for indoor location tracking

--- app/src/main/java/uk/ac/bath/mindpalaceapp/Main.java
+++ app/src/main/java/uk/ac/bath/mindpalaceapp/Main.java
@@ -2,12 +2,79 @@ package uk.ac.bath.mindpalaceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.estimote.indoorsdk.EstimoteCloudCredentials;
+import com.estimote.indoorsdk.IndoorLocationManagerBuilder;
+import com.estimote.indoorsdk_module.algorithm.OnPositionUpdateListener;
+import com.estimote.indoorsdk_module.algorithm.ScanningIndoorLocationManager;
+import com.estimote.indoorsdk_module.cloud.CloudCallback;
+import com.estimote.indoorsdk_module.cloud.CloudCredentials;
+import com.estimote.indoorsdk_module.cloud.EstimoteCloudException;
+import com.estimote.indoorsdk_module.cloud.IndoorCloudManager;
+import com.estimote.indoorsdk_module.cloud.IndoorCloudManagerFactory;
+import com.estimote.indoorsdk_module.cloud.Location;
+import com.estimote.indoorsdk_module.cloud.LocationPosition;
+import com.estimote.indoorsdk_module.view.IndoorLocationView;
 
 public class Main extends AppCompatActivity {
+
+    private final String APP_ID = "app_id";
+    private final String APP_TOKEN = "app_token";
+    private final String LOCATION_ID = "loc_id";
+    private CloudCredentials cloudCredentials;
+    private ScanningIndoorLocationManager indoorLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Use IndoorCloudManagerFactory to get objects for communicating with Estimote cloud.
+        cloudCredentials = new EstimoteCloudCredentials(APP_ID, APP_TOKEN);
+        IndoorCloudManager cloudManager = new IndoorCloudManagerFactory().create(this, cloudCredentials);
+        cloudManager.getLocation(LOCATION_ID, new CloudCallback<Location>() {
+            @Override
+            public void success(Location location) {
+                // ...do something with Location object... (needed to initialise IndoorLocationManager!)
+
+                // Use IndoorLocationView to display location on screen. It exists in activity_main.xml.
+                final IndoorLocationView indoorLocationView = findViewById(R.id.indoor_view);
+                indoorLocationView.setLocation(location);
+
+                // IndoorLocationManager
+                ScanningIndoorLocationManager indoorLocationManager =
+                        new IndoorLocationManagerBuilder(getApplicationContext(), location, cloudCredentials)
+                                .withDefaultScanner()
+                                .build();
+
+                indoorLocationManager.setOnPositionUpdateListener(new OnPositionUpdateListener() {
+                    @Override
+                    public void onPositionUpdate(LocationPosition locationPosition) {
+                        indoorLocationView.updatePosition(locationPosition);
+                    }
+
+                    @Override
+                    public void onPositionOutsideLocation() {
+                        indoorLocationView.hidePosition();
+                    }
+                });
+            }
+
+            @Override
+            public void failure(EstimoteCloudException e) {
+                // oops! Failure to get location from cloud.
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        indoorLocationManager.startPositioning();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        indoorLocationManager.stopPositioning();
     }
 }
