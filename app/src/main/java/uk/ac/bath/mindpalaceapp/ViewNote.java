d9287cc02e406e3602fafb6023995f8ef8b9a752
cd223 <chrisdavies870@gmail.com>
2019-01-29 11:58:10 +0000

    Starting view note with location

--- app/src/main/java/uk/ac/bath/mindpalaceapp/ViewNote.java
+++ app/src/main/java/uk/ac/bath/mindpalaceapp/ViewNote.java
@@ -1,8 +1,13 @@
 package uk.ac.bath.mindpalaceapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +23,18 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
+import com.estimote.indoorsdk.EstimoteCloudCredentials;
+import com.estimote.indoorsdk.IndoorLocationManagerBuilder;
+import com.estimote.indoorsdk_module.algorithm.OnPositionUpdateListener;
+import com.estimote.indoorsdk_module.algorithm.ScanningIndoorLocationManager;
+import com.estimote.indoorsdk_module.cloud.CloudCallback;
+import com.estimote.indoorsdk_module.cloud.CloudCredentials;
+import com.estimote.indoorsdk_module.cloud.EstimoteCloudException;
+import com.estimote.indoorsdk_module.cloud.IndoorCloudManager;
+import com.estimote.indoorsdk_module.cloud.IndoorCloudManagerFactory;
+import com.estimote.indoorsdk_module.cloud.Location;
+import com.estimote.indoorsdk_module.cloud.LocationPosition;
+import com.estimote.indoorsdk_module.view.IndoorLocationView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,15 +46,19 @@ public class ViewNote extends AppCompatActivity {
 
     private static final String url = "https://mindpalaceservice.herokuapp.com/nearestnote/";
     private static final String statusUrl = "https://mindpalaceservice.herokuapp.com/updatenotestatus/";
-
+    private final String APP_ID = "mind-palace-eew";
+    private final String APP_TOKEN = "3e6bcaf3b84c791b373a2bb439b3d239";
+    private final String LOCATION_ID = "1w2101";
+    private static final String TAG = ViewNote.class.getName();
     private static String palaceId;
 
     private double loc_x = 0.0;
     private double loc_y = 0.0;
     private double rad = 2.5;
 
+    private CloudCredentials cloudCredentials = new EstimoteCloudCredentials(APP_ID, APP_TOKEN);
+    private ScanningIndoorLocationManager indoorLocationManager;
     private final HashMap<String, String> noteTitleToId = new HashMap<>();
-    private static final String TAG = ViewNote.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +68,52 @@ public class ViewNote extends AppCompatActivity {
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_view_note);
         palaceId = getIntent().getStringExtra("palace_id");
-        viewNote();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) { checkPermission(); }
+
+        IndoorCloudManager cloudManager = new IndoorCloudManagerFactory().create(this, cloudCredentials);
+        cloudManager.getLocation(LOCATION_ID, new CloudCallback<Location>() {
+            @Override
+            public void success(Location location) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Location '" + location.getName() + "' loaded from Estimote Cloud.",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+
+                final IndoorLocationView indoorLocationView = findViewById(R.id.indoor_view_note_view);
+
+                indoorLocationManager = new IndoorLocationManagerBuilder(getApplicationContext(), location, cloudCredentials)
+                        .withPositionUpdateInterval(501L)
+                        .withDefaultScanner()
+                        .build();
+
+                indoorLocationManager.setOnPositionUpdateListener(new OnPositionUpdateListener() {
+                    @Override
+                    public void onPositionUpdate(LocationPosition locationPosition) {
+                        indoorLocationView.updatePosition(locationPosition);
+                        loc_x = locationPosition.getX();
+                        loc_y = locationPosition.getY();
+                        Log.d(TAG, "X: " + loc_x + " Y: " + loc_y);
+                        viewNote();
+                    }
+
+                    @Override
+                    public void onPositionOutsideLocation() {
+                        indoorLocationView.hidePosition();
+                    }
+                });
+
+                indoorLocationView.setLocation(location);
+                indoorLocationManager.startPositioning();
+            }
+
+            @Override
+            public void failure(EstimoteCloudException e) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "ERROR: Failed to get location from Estimote Cloud.",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 
     public void viewNote() {
@@ -142,5 +208,27 @@ public class ViewNote extends AppCompatActivity {
             }
         });
         queue.add(jsonRequest);
+    }
+
+    public void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                ){
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_ADMIN,
+                            Manifest.permission.INTERNET,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }
     }
 }
